dcda21dc75578e194dc047c504351ba7c3766955
rlygh <97500244+rlygh@users.noreply.github.com>
2022-08-04 07:53:48 +0000

    4

--- main.c
+++ main.c
@@ -1,26 +1,35 @@
 #include <stdio.h>
-int n,inp[1010],dp[1010],ans;
+int a,n,inp[1010],dp[1010],ans;
 
 int main()
 {
    scanf("%d", &n);
   for(int i=0; i<n; i++)
-    scanf("%d", &inp[i]);//입력
+    scanf("%d", &inp[i]);
   
   for (int i=0;i<n;i++)
     {
-      dp[i] =inp[i];
+      dp[i] =1;
       for (int j=0;j<i; j++)
         {
           if (inp[j]<inp[i])
           {
-            if (dp[i] <dp[j]+inp[i]) dp[i] =dp[j]+inp[i];
+            if (dp[i] <dp[j]+1) dp[i] =dp[j]+1;
           }
         }
     }
   for (int i=0; i<n; i++)
     {
-      if (ans<dp[i]) ans=dp[i];
-    }
+      if (ans<dp[i]){
+        ans=dp[i];
+        a=i;
+      }      
+      printf("%d",inp[i]);
+    }  
   printf("%d", ans);
+  for (int i=0;i<n;i++){
+    for(int j=0;j<i;j++){
+    if (inp[a]<inp[i]&&inp[i]!=inp[j])
+    }
+  }
 }
