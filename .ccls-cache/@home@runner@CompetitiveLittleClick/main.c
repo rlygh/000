5a07070a9641e61834c93dca00a3307f400633c8
rlygh <97500244+rlygh@users.noreply.github.com>
2022-08-04 05:46:59 +0000

    00

--- .ccls-cache/@home@runner@CompetitiveLittleClick/main.c
+++ .ccls-cache/@home@runner@CompetitiveLittleClick/main.c
@@ -7,16 +7,20 @@ int main()
   for(int i=0; i<n; i++)
     scanf("%d", &inp[i]);//입력
   
-  for (int i=0;i<n;i++){
-      dp[i] =1;
-      for (int j=0;j<i; j++){
-        if (inp[j]<inp[i]) ans=inp[i+j];
-        if (ans>dp[i]) dp[i]=ans;     
-      }
-    ans=0;
+  for (int i=0;i<n;i++)
+    {
+      dp[i] =inp[i];
+      for (int j=0;j<i; j++)
+        {
+          if (inp[j]<inp[i])
+          {
+            if (dp[i] <dp[j]+inp[i]) dp[i] =dp[j]+inp[i];
+          }
+        }
     }
-  for (int i=0; i<n; i++){
-    if(dp[i]>ans) ans=dp[i];
-    printf("%d ",ans);
-  } //printf("%d",ans);
+  for (int i=0; i<n; i++)
+    {
+      if (ans<dp[i]) ans=dp[i];
+    }
+  printf("%d", ans);
 }
